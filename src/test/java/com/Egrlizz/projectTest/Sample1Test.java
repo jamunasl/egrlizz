f81ce4057cfad59ff5f1dbaf7a125374c0e7955b
Admin <Admin@DESKTOP-O1RHDTI>
2022-05-30 15:26:15 +0530

    fist committ

---
+++ src/test/java/com/Egrlizz/projectTest/Sample1Test.java
@@ -0,0 +1,12 @@
+package com.Egrlizz.projectTest;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+	@Test
+	public void create1Test()
+	{
+		System.out.println("tc pass");
+	}
+
+}
---
+++ src/test/java/com/Egrlizz/projectTest/SampleTest2.java
@@ -0,0 +1,14 @@
+package com.Egrlizz.projectTest;
+
+import org.testng.annotations.Test;
+
+public class SampleTest2 {
+	@Test
+	public void createEgrlizzTest()
+	{
+		System.out.println("tc2 passs");
+	}
+	
+		
+	
+}
