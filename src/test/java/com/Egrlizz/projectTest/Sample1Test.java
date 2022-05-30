6a362f7c7c3e23ca16149d3ca09becb97229934f
jamunasl <106369127+jamunasl@users.noreply.github.com>
2022-05-30 18:09:14 +0530

    Update Sample1Test.java

--- src/test/java/com/Egrlizz/projectTest/Sample1Test.java
+++ src/test/java/com/Egrlizz/projectTest/Sample1Test.java
@@ -5,7 +5,9 @@ import org.testng.annotations.Test;
 public class Sample1Test {
 	@Test
 	public void create1Test()
-	{
+	{       
+             System.out.println(Browser);
+
 		System.out.println("tc pass");
 	        System.out.println("tc1 passs");
 
